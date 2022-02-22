1746efe78a6a8a4027dd16baa6ef4c3d14ddc875
Avihai Adler <avihai.adler@gmail.com>
2022-02-22 20:45:26 +0200

    added a log  operation

--- src/main/java/bot/listeners/UpdateLogChannelListener.java
+++ src/main/java/bot/listeners/UpdateLogChannelListener.java
@@ -4,34 +4,48 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.javacord.api.event.message.MessageCreateEvent;
 import org.javacord.api.listener.message.MessageCreateListener;
+import org.javacord.api.util.logging.ExceptionLogger;
+
 import bot.dal.DBManager;
 import bot.util.Misc;
 
 public class UpdateLogChannelListener implements MessageCreateListener {
   private Logger logger = LogManager.getLogger(UpdateLogChannelListener.class);
   private DBManager dbManager;
-  
+
   public UpdateLogChannelListener(DBManager dbManager) {
     this.dbManager = dbManager;
   }
-   
+
   @Override
   public void onMessageCreate(MessageCreateEvent event) {
-    if(event.getMessageAuthor().asUser().isEmpty()) return;
-    if(!Misc.isUserAllowed(event, event.getApi())) return;
-    
+    if (event.getMessageAuthor().asUser().isEmpty())
+      return;
+    if (!Misc.isUserAllowed(event, event.getApi()))
+      return;
+
     var content = event.getMessageContent().split("\\s+");
-    if(content.length < 2) return;
-    
+    if (content.length < 2)
+      return;
+
     var guild = dbManager.findGuildById(event.getServer().get().getIdAsString());
-    
+
     logger.info("invoking " + this.getClass().getName() + " for server " + guild.getId());
-    
-    if(Misc.channelExists(content[1], event.getServer().get())) {
+
+    if (Misc.channelExists(content[1], event.getServer().get())) {
       guild.setLogChannelId(content[1]);
       dbManager.upsert(guild);
-      
-      logger.info("the server " + guild.getId() + " changed their logging channel to " + guild.getLogChannelId());
+
+      if (event.getChannel().canYouWrite()) {
+        logger.info("the server " + guild.getId() + " changed their logging channel to " + guild.getLogChannelId());
+        event.getChannel()
+            .sendMessage("Logs will appear at **"
+                + event.getServer().get()
+                    .getChannelById(guild.getLogChannelId()).get()
+                    .getName()
+                + "**")
+            .exceptionally(ExceptionLogger.get());
+      }
     }
   }
 
