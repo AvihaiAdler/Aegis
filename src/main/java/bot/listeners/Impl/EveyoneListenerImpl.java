b073429de9356ec6f4adc79d96b20cf99721c6c1
Avihai Adler <avihai.adler@gmail.com>
2022-03-02 20:44:18 +0200

    fixed an issue on servers with suppress @everyone

--- src/main/java/bot/listeners/Impl/EveyoneListenerImpl.java
+++ src/main/java/bot/listeners/Impl/EveyoneListenerImpl.java
@@ -10,7 +10,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import bot.dal.SpamUrlsDao;
 import bot.data.GuildEntity;
 import bot.data.SpamUrlEntity;
@@ -39,39 +38,40 @@ public class EveyoneListenerImpl implements EveryoneListener {
     // guild isn't restricted
     if(!guild.getRestricted()) return;
     
-    // didn't tag everyone
-    if (!event.getMessage().mentionsEveryone()) return;
-
-    // message contains a URL / Embed
-    if (!event.getMessage().getEmbeds().isEmpty() || Misc.containsUrl(event.getMessageContent())) {
-      event.deleteMessage().exceptionally(e -> {
-        logger.error("failed to delete a message from " + event.getChannel().getId()
-                + " server " + guild.getGuildName() + "(" + guild.getId() + ")" + "\nreason: " + Misc.parseThrowable(e));
-        return null;
-      });
-      
-      //save the potential spam message in the db
-      Misc.getUrls(event.getMessageContent()).forEach(url -> {
-        spamUrlsDao.save(new SpamUrlEntity(Instant.now().getEpochSecond(), url, event.getServer().get().getIdAsString()));        
-      });
-      
-      logger.warn("detected spam message for server " + guild.getId() + " in channel "
-              + event.getChannel().getIdAsString() + "\noriginal message " + event.getMessageContent());
-
-      // log to the log channel
-      var logChannelId = guild.getLogChannelId();
-
-      // logging channel doesn't exists
-      if (!Misc.channelExists(logChannelId, event.getServer().get())) return;
-      
-      // log the delete action
-      var now = ZonedDateTime.now(ZoneId.of(ZoneOffset.UTC.toString()));
-      var message = DateTimeFormatter.ofPattern("dd/MM/uuuu, HH:mm:ss").format(now) + " (UTC): a message from **"
-              + event.getMessageAuthor().getDiscriminatedName() + "** (`" + event.getMessageAuthor().getIdAsString() + "`)" 
-              + " in **#" + event.getServerTextChannel().get().getName() + "** (`" + event.getChannel().getId()
-              + "`) was deleted by **" + event.getApi().getYourself().getDiscriminatedName()
-              + "**. Reason: ```possible spam```";
-      messageSender.send(event.getServer().get().getTextChannelById(logChannelId).get(), message, guild);
-    }   
+    // tag everyone
+    if (event.getMessageContent().contains("@everyone") || event.getMessage().mentionsEveryone()) {
+      System.err.println("url? " + Misc.containsUrl(event.getMessageContent()));
+      // message contains a URL / Embed
+      if (!event.getMessage().getEmbeds().isEmpty() || Misc.containsUrl(event.getMessageContent())) {
+        event.deleteMessage().exceptionally(e -> {
+          logger.error("failed to delete a message from " + event.getChannel().getId()
+                  + " server " + guild.getGuildName() + "(" + guild.getId() + ")" + "\nreason: " + Misc.parseThrowable(e));
+          return null;
+        });
+        
+        //save the potential spam message in the db
+        Misc.getUrls(event.getMessageContent()).forEach(url -> {
+          spamUrlsDao.save(new SpamUrlEntity(Instant.now().getEpochSecond(), url, event.getServer().get().getIdAsString()));        
+        });
+        
+        logger.warn("detected spam message for server " + guild.getId() + " in channel "
+                + event.getChannel().getIdAsString() + "\noriginal message " + event.getMessageContent());
+        
+        // log to the log channel
+        var logChannelId = guild.getLogChannelId();
+        
+        // logging channel doesn't exists
+        if (!Misc.channelExists(logChannelId, event.getServer().get())) return;
+        
+        // log the delete action
+        var now = ZonedDateTime.now(ZoneId.of(ZoneOffset.UTC.toString()));
+        var message = DateTimeFormatter.ofPattern("dd/MM/uuuu, HH:mm:ss").format(now) + " (UTC): a message from **"
+                + event.getMessageAuthor().getDiscriminatedName() + "** (`" + event.getMessageAuthor().getIdAsString() + "`)" 
+                + " in **#" + event.getServerTextChannel().get().getName() + "** (`" + event.getChannel().getId()
+                + "`) was deleted by **" + event.getApi().getYourself().getDiscriminatedName()
+                + "**. Reason: ```possible spam```";
+        messageSender.send(event.getServer().get().getTextChannelById(logChannelId).get(), message, guild);
+      }      
+    } 
   }
 }
